be00e8d70329c2bd1006b20ac842c0006c6d0da8
Dan Cobb <dancobb@gmail.com>
2017-02-20 17:40:49 -0500

    unit tests

--- test/__utilTool.js
+++ test/__utilTool.js
@@ -1,5 +1,18 @@
 const v = require('../src/updater.js');
+const expect = require('chai').expect;
 
 describe('Util Tool DEBUG', () => {
-  it('Should write to file', () => {})
+  it('Should write to file', () => {});
+});
+describe('Testing manual update', () => {
+  it('Should return a string', () => {
+    v.update.updateManual('1.0.0', 'test');
+    expect('1.0.0');
+  });
+});
+describe('Testing Auto update', () => {
+  it('Should return a string', () => {
+    v.update.updateAuto('test');
+    expect('package.json updated');
+  });
 });
