98d9b313f2e698208d4b5bbd237fd2b79b81957c
Dan Cobb <dancobb@gmail.com>
2017-02-20 19:13:56 -0500

    removed class for stdout

--- src/utilTool.js
+++ src/utilTool.js
@@ -1,33 +1,18 @@
 const colors = require('colors');
 
-class console {
-  log(data) {
-    this.data = data;
-    process.stdout.write(`${this.data}\n`);
-  }
-  error(data) {
-    this.data = data;
-    process.stderr.write(`${this.data}\n`);
-  }
-  warn(data) {
-    this.data = data;
-    process.stderr.write(`${this.data}\n`);
-  }
-}
 module.exports = {
   msg(m, t) {
     const msg = m;
     let type = t;
     const time = new Date();
     const border = '\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n';
-    const con = new console();
     if (type === 'log') {
-      con.log(`${border}~~~~~~~~ ${type.toUpperCase()} Event ~~~~~~~~\n${msg}\n${time}`.green);
+      console.log(`${border}~~~~~~~~ ${type.toUpperCase()} Event ~~~~~~~~\n${msg}\n${time}`.green);
     } else if (type === 'warn') {
       type = 'Warning';
-      con.warn(`${border}~~~~~~~~ ${type.toUpperCase()} Event ~~~~~~~~\n${msg}\n${time}`.yellow);
+      console.warn(`${border}~~~~~~~~ ${type.toUpperCase()} Event ~~~~~~~~\n${msg}\n${time}`.yellow);
     } else if (type === 'error') {
-      con.error(`${border}~~~~~~~~ ${type.toUpperCase()} Event ~~~~~~~~\n${msg}\n${time}`.red);
+      console.error(`${border}~~~~~~~~ ${type.toUpperCase()} Event ~~~~~~~~\n${msg}\n${time}`.red);
     }
   },
 };
