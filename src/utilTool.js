2c3bfa0d3950d6613e8dbbdc17a79ef2b67e1e0a
Dan Cobb <dancobb@gmail.com>
2017-02-20 19:32:43 -0500

    added dot env and check for debug

--- src/utilTool.js
+++ src/utilTool.js
@@ -1,4 +1,5 @@
 const colors = require('colors');
+require('dotenv').config;
 
 module.exports = {
   msg(m, t) {
@@ -6,12 +7,12 @@ module.exports = {
     let type = t;
     const time = new Date();
     const border = '\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n';
-    if (type === 'log') {
+    if (type === 'log' && process.env.DEBUG) {
       console.log(`${border}~~~~~~~~ ${type.toUpperCase()} Event ~~~~~~~~\n${msg}\n${time}`.green);
-    } else if (type === 'warn') {
+    } else if (type === 'warn' && process.env.DEBUG) {
       type = 'Warning';
       console.warn(`${border}~~~~~~~~ ${type.toUpperCase()} Event ~~~~~~~~\n${msg}\n${time}`.yellow);
-    } else if (type === 'error') {
+    } else if (type === 'error' && process.env.DEBUG) {
       console.error(`${border}~~~~~~~~ ${type.toUpperCase()} Event ~~~~~~~~\n${msg}\n${time}`.red);
     }
   },
